e5bfbee9bfe492c49a396b28294611199439a20f
Ubuntu <ubuntu@ip-172-31-8-255.ap-northeast-1.compute.internal>
2025-12-23 06:56:02 +0000

    2ndデプロイ+テスト完了

--- app/javascript/packs/posts_raty.js
+++ app/javascript/packs/posts_raty.js
@@ -1,19 +1,22 @@
-document.addEventListener('turbolinks:load', function() {
-  // クラス指定で要素を取得
-  let elem = document.querySelector('.post_raty');
-  if (elem) {
-    let hiddenFieldId = elem.dataset.targetRating; // data-target-ratingの値を取得
-    let hiddenField = document.getElementById(hiddenFieldId);
+document.addEventListener('turbolinks:load', function () {
+  let elem = document.querySelector('.post_raty')
+  if (!elem) return
 
-    raty(elem, {
-      number: 5,
-      path: '/assets/raty',
-      score: hiddenField.value || 0,
-      click: function(score) {
-        hiddenField.value = score;
-      },
-      half: true,   // ★ これが必要
-      hints: ['1','2','3','4','5']
-    });
-  }
-});
+  let hiddenFieldId = elem.dataset.targetRating
+  let hiddenField = document.getElementById(hiddenFieldId)
+
+  raty(elem, {
+    number: 5,
+    score: hiddenField.value || 0,
+
+    // ★ ここが超重要
+    path: '/assets/raty',
+
+    half: true,
+    hints: ['1', '2', '3', '4', '5'],
+
+    click: function (score) {
+      hiddenField.value = score
+    }
+  })
+})
