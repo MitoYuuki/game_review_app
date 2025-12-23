e342c25dd8b00f6f0a72cc32bb56afe6b5346e23
Ubuntu <ubuntu@ip-172-31-8-255.ap-northeast-1.compute.internal>
2025-12-23 06:56:02 +0000

    2ndデプロイ+テスト完了

--- app/javascript/packs/posts_raty.js
+++ app/javascript/packs/posts_raty.js
@@ -1,19 +1,27 @@
-document.addEventListener('turbolinks:load', function() {
-  // クラス指定で要素を取得
-  let elem = document.querySelector('.post_raty');
-  if (elem) {
-    let hiddenFieldId = elem.dataset.targetRating; // data-target-ratingの値を取得
-    let hiddenField = document.getElementById(hiddenFieldId);
+import starOn from '../images/raty/star-on.png'
+import starOff from '../images/raty/star-off.png'
+import starHalf from '../images/raty/star-half.png'
 
-    raty(elem, {
-      number: 5,
-      path: '/assets/raty',
-      score: hiddenField.value || 0,
-      click: function(score) {
-        hiddenField.value = score;
-      },
-      half: true,   // ★ これが必要
-      hints: ['1','2','3','4','5']
-    });
-  }
-});
+document.addEventListener('turbolinks:load', function () {
+  let elem = document.querySelector('.post_raty')
+  if (!elem) return
+
+  let hiddenFieldId = elem.dataset.targetRating
+  let hiddenField = document.getElementById(hiddenFieldId)
+
+  raty(elem, {
+    number: 5,
+    score: hiddenField.value || 0,
+
+    starOn: starOn,
+    starOff: starOff,
+    starHalf: starHalf,
+
+    half: true,
+    hints: ['1', '2', '3', '4', '5'],
+
+    click: function (score) {
+      hiddenField.value = score
+    }
+  })
+})
