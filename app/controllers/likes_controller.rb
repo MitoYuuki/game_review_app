2b5322c3c5c8d80ba49c0c2ddb6b36a98cc04eba
Ubuntu <ubuntu@ip-172-31-8-255.ap-northeast-1.compute.internal>
2025-12-28 10:05:55 +0000

    20251228 Implement asynchronous like feature with Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,20 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.likes.create(post_id: @post.id)
-
+    @like = current_user.likes.create(post_id: @post.id)
+    
     respond_to do |format|
+      format.js   # JavaScript形式のレスポンスを追加
       format.html { redirect_to @post }
     end
   end
 
   def destroy
-    current_user.likes.find_by(post_id: @post.id)&.destroy
-
+    @like = current_user.likes.find_by(post_id: @post.id)
+    @like&.destroy
+    
     respond_to do |format|
+      format.js   # JavaScript形式のレスポンスを追加
       format.html { redirect_to @post }
     end
   end
@@ -26,8 +29,15 @@ class LikesController < ApplicationController
 
   def reject_guest_user
     if current_user&.guest?
-      redirect_back fallback_location: root_path,
-        alert: "ゲストユーザーはいいねできません"
+      respond_to do |format|
+        format.js do
+          render js: "alert('ゲストユーザーはいいねできません');"
+        end
+        format.html do
+          redirect_back fallback_location: root_path,
+            alert: "ゲストユーザーはいいねできません"
+        end
+      end
     end
   end
 end
