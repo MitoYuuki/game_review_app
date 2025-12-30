f25d2ff0aab8dfa2b1c5aa47ab16299e15007158
Ubuntu <ubuntu@ip-172-31-8-255.ap-northeast-1.compute.internal>
2025-12-30 06:10:25 +0000

    20251230_Add responsive layouts and UI adjustments for posts, users, and top page

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @posts = Post.includes(:user).order(created_at: :asc)
+    @posts = Post.includes(:user).order(:id)
   end
 
   def show
