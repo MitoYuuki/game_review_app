f481fb2529b774ea389b8a1b29c561e58384f6f2
Ubuntu <ubuntu@ip-172-31-8-255.ap-northeast-1.compute.internal>
2026-02-28 06:48:01 +0000

    Fix syntax error in Admin::CommentsController#destroy

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,15 +3,16 @@ class Admin::CommentsController < Admin::BaseController
     @comment = Comment.find_by(id: params[:id])
     
     unless @comment
-    redirect_to admin_posts_path, alert: "コメントが見つかりません"
-    return
-  end
+      redirect_to admin_posts_path, alert: "コメントが見つかりません"
+      return
+    end
 
-  @post = @comment.post
+    @post = @comment.post
 
-  if @comment.destroy
-    redirect_to admin_post_path(@post), notice: "コメントを削除しました"
-  else
-    redirect_to admin_post_path(@post), alert: "コメントの削除に失敗しました"
+    if @comment.destroy
+      redirect_to admin_post_path(@post), notice: "コメントを削除しました"
+    else
+      redirect_to admin_post_path(@post), alert: "コメントの削除に失敗しました"
+    end
   end
 end
